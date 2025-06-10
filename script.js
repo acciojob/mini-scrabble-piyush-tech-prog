c4a718e55d05e88f3727f0018b3dcccf360326f2
Piyush Sharma <piyushopsaws@gmail.com>
2025-06-10 18:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 let count =0
         document.getElementById("evaluatedText").addEventListener("input",(e)=>{
-            if(e.inputType === "insertText"){
-            count++
+            count = e.target.value.length
             document.getElementById("letterCount").innerText = `${count}`
-            }else if(e.inputType === "deleteContentBackward"){
-                count--
-                document.getElementById("letterCount").innerText = `${count}`
-            }
         })
